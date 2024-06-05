2418db77b224f3d6905bd03e1a1d3518c884d0e1
psalmistcom <dadewale9@gmail.com>
2024-06-05 15:32:59 +0100

    hero section

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Layout from "@/Layouts/Layout";
+import Hero from "@/Components/Hero";
 import { Link, Head } from "@inertiajs/react";
 
 export default function Welcome({ auth }) {
@@ -6,6 +7,10 @@ export default function Welcome({ auth }) {
         <>
             <Layout auth={auth}>
                 <Head title="Welcome" />
+                <Hero
+                    latestProperty="3 Bedroom Detached Bungalow"
+                    propLink="dashboard"
+                />
             </Layout>
         </>
     );
